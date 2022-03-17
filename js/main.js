3ea41d26f4c110b8aad69cf2aa6710a9e75ba08a
ArslonRadjabov <arslonradjabovblog@gmail.com>
2022-03-17 11:06:14 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,47 +1,112 @@
-// const adder = document.getElementsByClassName('menu__btn');
-// console.log(adder);
 
-// for (let i = 0; i < adder.length; i++) {
-//     let button = adder[i];
-//     button.addEventListener('click', function (event) {
-//         var buttonclicked = event.target
-//         updateCardTotal();
-//     })
-    
-// }
-
-var addToCard = document.getElementsByClassName('menu__btn');
-for (let i = 0; i < addToCard.length; i++) {
-    const button = addToCard[i];
-    button.addEventListener('click', cardClicked);
-}
-
-function cardClicked(event) {
-    var button = event.target;
-    var menu__item = button.parentElement.parentElement;
-    var title  = menu__item.getElementsByClassName('menu__title')[0].innerText;
-    var price = menu__item.getElementsByClassName('menu__cost')[0].innerText;
-    var img = menu__item.getElementsByClassName('menu__img')[0].src;
-    console.log(title,price,img);
-    addItemtoCard(title, price,img);
-}
-
-function addItemtoCard(title, price,img) {
-    var cartRow = document.createElement('div');
-    cartRow.classList.add('menu__item');
-    
-}
-
-// function updateCardTotal() {
-//     var cardMenu = document.getElementsByClassName('menu')[0];
-//     var cartRows = cardMenu.getElementsByClassName('menu__item')[0];
-//     var total = 0;
-//     for (let i = 0; i < cartRows.length; i++) {
-//         let cartRow = cartRows[i];
-//         let priceElement = cartRow.getElementById('menu__cost');
-//         let narx = priceElement.value;
-//         // var price = parseFloat(priceElement.innerText.replace('$', ''))
-//         total = total + narx;
-//     }  
-//     document.getElementsByClassName('total__price')[0].innerText = total;
-// }
+let pizzas = [
+    {
+      id: 1,
+      imageUrl: "../images/pizza1.jpg",
+      name: "Pepperoni",
+      price: 2.23,
+    },
+    {
+      id: 2,
+      imageUrl: "../images/pizza2.jpg",
+      name: "Cheesy",
+      price: 5.99,
+    },
+    {
+      id: 3,
+      imageUrl: "../images/pizza3.jpg",
+      name: "Margarita",
+      price: 7.48,
+    },
+    {
+      id: 4,
+      imageUrl: "../images/pizza4.jpg",
+      name: "Hawaiian",
+      price: 9.32,
+    },
+  ];
+  
+  let menuList = document.querySelector(".menu");
+  let cartList = document.querySelector(".card__list");
+  let elSubTotal = document.querySelector("#subtotal");
+  
+  let totalPrice = 0;
+  
+  // left box added
+  for (let i = 0; i < pizzas.length; i++) {
+    let li = document.createElement("li");
+    li.className = "menu__item pizza";
+    li.innerHTML = `
+        <img class="pizza__img" src=${pizzas[i].imageUrl} alt="pizza" />
+              <div class="pizza__name">
+                <p class="pizza__p">${pizzas[i].name}</p>
+                <span class="pizza__price">$${pizzas[i].price}</span>
+                <button class="pizza__btn" onclick='addItem(${pizzas[i].id} )'>
+                  Add to Cart
+                </button>
+              </div>`;
+  
+    menuList.appendChild(li);
+  }
+  
+  let newPizzasArr = [];
+  function addItem(id) {
+    for (let i = 0; i < pizzas.length; i++) {
+      if (id == pizzas[i].id) {
+        newPizzasArr.push(pizzas[i]);
+      }
+    }
+  
+    for (let i = 0; i < newPizzasArr.length; i++) {
+      if (i == newPizzasArr.length - 1) {
+        let li = document.createElement("li");
+        li.className = "menu__item pizza cart__item";
+        li.innerHTML = `
+        <img class="pizza__img" src=${newPizzasArr[i].imageUrl} alt="pizza" />
+              <div class="pizza__name">
+                <p class="pizza__p">${newPizzasArr[i].name}</p>
+                <span class="pizza__price">$${newPizzasArr[i].price}</span>
+                <button class="pizza__btn remove"  onclick='removeItem(${i})'>
+                  -
+                </button>
+              </div>
+      `;
+        totalPrice += newPizzasArr[i].price;
+        elSubTotal.textContent = totalPrice.toFixed(2);
+        cartList.appendChild(li);
+      }
+    }
+  }
+  
+  function removeItem(index) {
+    let newArrRemove = [];
+  
+    for (let i = 0; i < newPizzasArr.length; i++) {
+      if (index != i) {
+        newArrRemove.push(newPizzasArr[i]);
+      }
+    }
+  
+    newPizzasArr = newArrRemove;
+  
+    cartList.innerHTML = "";
+    totalPrice = 0;
+  
+    for (let i = 0; i < newPizzasArr.length; i++) {
+      let li = document.createElement("li");
+      li.className = "menu__item pizza cart__item";
+      li.innerHTML = `
+        <img class="pizza__img" src=${newPizzasArr[i].imageUrl} alt="pizza" />
+              <div class="pizza__name">
+                <p class="pizza__p">${newPizzasArr[i].name}</p>
+                <span class="pizza__price">$${newPizzasArr[i].price}</span>
+                <button class="pizza__btn remove"  onclick='removeItem(${i})'>
+                  -
+                </button>
+              </div>
+      `;
+      totalPrice += newPizzasArr[i].price;
+      elSubTotal.textContent = totalPrice.toFixed(2);
+      cartList.appendChild(li);
+    }
+  }
